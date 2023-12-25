6f287b95ae81e8c2fa0086cb9a506002891fa759
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-12-25 14:49:39 +0600

    User API search with regExp added

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -2,7 +2,22 @@ const User = require("../models/user.model");
 
 const getUser = async (req, res, next) => {
   try {
-    const users = await User.find();
+    const search = req.query.search || "";
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 5;
+
+    const searchRegExp = new RegExp(".*" + search + ".*", "i");
+
+    const filter = {
+      isAdmin: { $ne: true },
+      $or: [
+        { name: { $regex: searchRegExp } },
+        { email: { $regex: searchRegExp } },
+        { phone: { $regex: searchRegExp } },
+      ],
+    };
+
+    const users = await User.find(filter);
     res.status(200).send({
       message: "users ware returned",
       users,
@@ -12,3 +27,7 @@ const getUser = async (req, res, next) => {
   }
 };
 module.exports = getUser;
+
+// db.example.find({
+//   $or: [{ x: { $eq: 0 } }, { $expr: { $eq: [{ $divide: [1, "$x"] }, 3] } }],
+// });
